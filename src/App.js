c52ac10df07b7d2379995b278992da938c4f7f4d
iris herholz <irisherholz@MacBook-Pro-von-iris.local>
2022-12-20 15:00:18 +0100

    createForm

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import "./App.css";
+import { useState } from "react";
 import Form from "./Components/Form.js";
+import { uid } from "uid";
 
 function App() {
+  const [activities, setActivities] = useState([]);
+  console.log(activities);
   function handleAddActivity(event) {
     event.preventDefault();
-    const formData = new FormData(event.target);
-    const data = Object.fromEntries(formData);
+    console.log(event.target.elements.isGood.checked);
+    setActivities([
+      ...activities,
+      {
+        name: event.target.elements.name.value,
+        isGood: event.target.elements.isGood.checked,
+        id: uid(),
+      },
+    ]);
+    //event.target.reset()
+    //event.target.data.name.focus()
   }
   return (
     <div className="App">
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -3,9 +3,9 @@ export default function Form({ onAddActivity }) {
     <form onSubmit={onAddActivity}>
       <h2>Add new Activity</h2>
       <label htmlFor="name">Name:</label>
-      <input type="text" id="name" name="name"></input>
+      <input type="text" id="name" name="name" />
       <label htmlFor="isGood">Good Weather Activity!</label>
-      <input type="checkbox" id="isGood" name="isGood"></input>
+      <input type="checkbox" id="isGood" name="isGood" />
       <button type="submit">submit</button>
     </form>
   );
